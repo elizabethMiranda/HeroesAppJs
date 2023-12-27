425888b24c981829e0dc594982d5cebb0496c565
elizabethmiranda <mirandaeli23@gmail.com>
2023-12-26 22:46:24 -0600

    probando ...

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,30 +1,28 @@
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
-import { useContext } from 'react';
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 export const LoginPage = () => {
-    const navigate = useNavigate();
-//para hacer uso del context lo importamos
-const { login } = useContext(AuthContext)
+  const navigate = useNavigate();
+  //para hacer uso del context lo importamos
+  const { login } = useContext(AuthContext);
 
-    const onLogin = () =>{
-        const lastPath = localStorage.getItem('lastPath') || '/';
+  const onLogin = () => {
+    const lastPath = localStorage.getItem("lastPath") || "/";
 
-        login('Elizabeth Miranda');
+    login("Elizabeth Miranda Cortez");
 
-        navigate(lastPath,{
-            replace: true
-        });
-    }
+    navigate(lastPath, {
+      replace: true
+    });
+  };
   return (
-   <div className="container mt-5">
-        <h1>Login</h1>
-        <hr />
-        <button className="btn btn-primary"
-        onClick={onLogin}
-        >
-            Login
-        </button>
-   </div>
-  )
-}
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={onLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
